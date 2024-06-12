5bf41341458e3f0db9a98feafb3f31c56d01b925
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-12 11:05:05 -0300

    Pastas 7 e 8 boolean e null e undefined respectivamente

---
+++ 8_null-e-undefined/js/script.js
@@ -0,0 +1,14 @@
+//hoisting - içamento
+console.log(sobreNome);
+console.log(numero)
+
+var nome = null;
+var sobreNome = "Santos"
+
+console.log(nome);
+
+nome = "Alexsandro";
+
+console.log(nome);
+
+var numero = 5;
