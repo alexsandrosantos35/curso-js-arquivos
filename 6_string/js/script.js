6a4c3ff2b5f03f07b74be1fbedf617d7ed9edf85
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-12 10:09:32 -0300

    Correção nas pastas 4 e 5 e inclusão da pasta 6

---
+++ 6_string/js/script.js
@@ -0,0 +1,25 @@
+var nome = "Alexsandro";
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+var sobreNome = "Santos";
+
+var nomeCompleto = nome + " " + sobreNome;
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase complexa.'
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse "Olá"');
+
+document.write("Ele disse 'Olá'");
