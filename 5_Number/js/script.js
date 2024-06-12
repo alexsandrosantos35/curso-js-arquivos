bf331f332d03339df93ae62963c31fd4a27acef1
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-11 21:52:52 -0300

    Adiciona os aarquivos da pasta Number

---
+++ 5_Number/js/script.js
@@ -0,0 +1,23 @@
+var teste = 1;
+
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.56;
+
+console.log(float);
+console.log(typeof float);
+
+var textoComNumero = "345";
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+console.log(NaN);
+
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
+
