5b1309f55b7c6e1a2f933f7162f44d5a36ee1562
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-13 07:03:37 -0300

    Pastas 11 e 12

---
+++ 10_Arrays/js/script.js
@@ -0,0 +1,15 @@
+var vetor = [5, "Alexsandro", true, {teste1: 12, teste2: true}];
+
+console.log(vetor);
+
+var vetor2 = [2, 11, 34, 22, 23];
+
+console.log(vetor2);
+
+console.log(vetor[1]);
+console.log(vetor2[0]);
+
+vetor[4] = 10;
+vetor[0] = "teste";
+
+console.log(vetor);
---
+++ 11_if/js/script.js
@@ -0,0 +1,13 @@
+var idade = 15;
+
+console.log("Antes de if.");
+
+if(idade >= 18) {
+    console.log("Pode fazer a carteira de habilitação");  
+}
+
+if(idade == 15) {
+    console.log("Precisa esperar três anos ainda.");
+}
+
+console.log("Depois do if.");
