6bd74155ae61b29758623e338ed3a669d73830a5
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-13 09:08:19 -0300

    pasta 14 operadores logicos segunda parte

---
+++ 14_op-comparacao2/js/script.js
@@ -0,0 +1,9 @@
+var numero = "5";
+
+if(numero === 5) {
+    console.log("É o numero cinco e é do tipo Number");
+}
+
+if(numero !== 5) {
+    console.log("Não é o numero cinco e é não do tipo Number"); 
+}
