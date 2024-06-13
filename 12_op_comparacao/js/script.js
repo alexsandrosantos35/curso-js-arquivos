d402af8bcc4a931367079d758db676730ea2600a
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-13 07:48:10 -0300

    Pasta 12

---
+++ 12_op_comparacao/js/script.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var possuiCarro = 1;
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer a carteira.")
+}
+
+if(idade <=  17) {
+    console.log("O usuário não pode fazer a carteira.")
+}
+
+if(possuiCarro) {
+    console.log("O usuário já pode andar neste carro.")
+}
+
+var nome = "Pedro";
+
+if(nome == "Alexsandro") {
+    console.log("Seu nome é Alexsandro");
+}
+
+if(nome != "Alexsandro") {
+    console.log("Seu nome não é Alexsandro");
+}
+
+if(20 > 10) {
+    console.log("Passou");
+}
+
+if(100 < 20) {
+    console.log("Passou2");
+}
