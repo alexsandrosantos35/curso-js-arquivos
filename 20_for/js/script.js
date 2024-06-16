f65c9af9be0db610a706ef3b1d752e33d6346df4
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-16 08:21:14 -0300

    Pasta 20 for

---
+++ 20_for/js/script.js
@@ -0,0 +1,15 @@
+for(var i=0; i<6; i++) {
+    console.log("Repetindo com for " + i);
+}
+
+var vetor = [1, 2, 3, 4];
+
+for(var j=0; j<vetor.length; j++) {
+    console.log(vetor[j]);
+}
+
+console.log("Tamanho de vetor = " + vetor.length);
+
+for(var x=1; x<100; x*=3) {
+    console.log(x);
+}
