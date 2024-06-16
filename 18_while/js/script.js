969599f5de2a2ea8608e5ae8afaca7de865329a4
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-16 07:38:03 -0300

    Pastas 18 e19 while e ops. de atribuição

---
+++ 18_while/js/script.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+while(x <= 5) {
+    console.log("Testando repetição " + x);
+    x++;
+}
+
+var vetor = ["teste", "Testando", "a", "b"]; 
+var i = 0;
+
+while(i < 4) {
+    console.log("Elemento " + i + " " + vetor[i]);
+    i++;
+}
+
+var palavra = "Matheus";
+var j = 0;
+
+while(j <= 6) {
+    console.log(palavra[i]);
+    j++;
+}
---
+++ 19_op-atribuição/js/script.js
@@ -0,0 +1,27 @@
+var x = 1;
+var y = 2;
+
+console.log(x = x + y);
+console.log(x += y);
+
+console.log(x -= y);
+
+console.log(x *= y);
+
+console.log(x /= y);
+
+console.log(x++);
+console.log(x--);
+
+while(x < 100) {
+    console.log(x);
+
+    x *= 2;
+}
+
+console.log(x);
+
+while(x > 0) {
+    console.log(x);
+    x -= 5;
+}
