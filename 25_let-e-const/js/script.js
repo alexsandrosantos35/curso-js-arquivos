7d3bc4830737cbbe98e75ec46a24285990148732
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-17 09:21:13 -0300

    Pastas 25 e 26 let e const e metodos numericos respectivalente

---
+++ 25_let-e-const/js/script.js
@@ -0,0 +1,28 @@
+let x = 20;
+
+const y = 10;
+//y = 12;
+console.log(y);
+
+x = 12;
+
+console.log(x);
+
+if(true) {
+    let x = 1;
+    console.log(x);
+
+    const y = 50;
+    console.log(y);
+}
+
+console.log(x);
+
+if(20 > 10) {
+    const y = 5;
+    console.log(y);
+}
+
+for(let x=0; x<10; x++) {
+    console.log(x);
+}
