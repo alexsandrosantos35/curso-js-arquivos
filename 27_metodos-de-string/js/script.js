d01d3768902c836bc4477e892b784924a3b8b399
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-17 10:07:31 -0300

    Pasta 27 metodos de string

---
+++ 27_metodos-de-string/js/script.js
@@ -0,0 +1,29 @@
+//length
+
+let nome = "Matheus";
+
+console.log(nome.length);
+
+let obj = "bola"
+
+console.log(obj.length);
+
+//indexOf 
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma"
+
+console.log(frase.indexOf("roeu"));
+
+///slice
+
+let roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+//replace
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
