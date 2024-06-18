cd77d295916035b6e3b9b851e99f7afa58337e30
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-18 15:43:43 -0300

    Pasta 35 alterando conteudo

---
+++ 35_alterando-conteudo/js/script.js
@@ -0,0 +1,15 @@
+//Selecionar o elemento
+
+var titulo = document.getElementById("title");
+
+console.log(titulo);
+
+//innerHTML
+
+titulo.innerHTML = "Texto alterado pelo innerHTML";
+
+//textContent
+
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.textContent = "Texto alterado pelo textContent";
