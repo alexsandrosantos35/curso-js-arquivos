effd50014405e871542ab691a93f23c0bab1c610
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-18 14:22:26 -0300

    Pasta 33 acessando o dom

---
+++ 33_acessando-o-dom/js/script.js
@@ -0,0 +1,23 @@
+//tag
+
+let titulo = document.getElementsByTagName("h1")[0];
+
+console.log(titulo);
+
+let lista = document.getElementsByTagName("li");
+
+console.log(lista);
+console.log(lista[3]);
+
+//id
+
+var paragrafo = document.getElementById("paragrafo");
+
+console.log(paragrafo);
+
+//classe
+
+let item = document.getElementsByClassName("item");
+
+console.log(item);
+console.log(item[2]);
