904a6be67273c2fc3fa21f99ded32979b19a01fb
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-19 11:36:16 -0300

    Pasta 41 adcionando css

---
+++ 41_adcionando-css/js/script.js
@@ -0,0 +1,14 @@
+// selecionar o elemento
+
+var title = document.querySelector("#title");
+
+// aplicar o estilo
+
+title.style.color = "red";
+title.style.backgroundColor = "yellow";
+
+// selecionar o elemento
+
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
