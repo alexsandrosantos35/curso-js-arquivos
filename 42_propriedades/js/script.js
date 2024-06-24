500f7cbfbd7cf7847c148493f37ab7d709fb2a9d
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-24 16:22:07 -0300

    Psata 42 propriedades do documento

---
+++ 42_propriedades/js/script.js
@@ -0,0 +1,18 @@
+// propriedades do documento
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links[0]);
+
+document.links[0].textContent = "twitter";
+
+console.log(document.URL);
+
+console.log(document.title);
+
+document.title = "Aula 42";
+
+console.log(document.title);
+
