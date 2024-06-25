11eda1731168d275fb40c603e5967acef06b9213
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-25 09:41:28 -0300

    Pasta 46 evento onload

---
+++ 46_evento-onload/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    console.log("O dom carregou");
+
+    let title = document.querySelector("#title");
+
+    console.log(title);
+}
+
+console.log("O js carregou");
+
+var title2 = document.querySelector("#title");
+
+console.log(title2);
