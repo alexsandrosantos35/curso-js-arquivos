3f14497b5f8853a558004dcc91f7d09a9e570d79
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2024-06-26 10:28:46 -0300

    Pasta 49 keydown e keyup

---
+++ 49_keydown-keyup/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        console.log("Teclou Enter");
+    }
+});
+
+document.addEventListener("keyup", function(e) {
+    if(e.key === "Enter") {
+        console.log("Soltou o Enter");
+    }
+});
